8a9a5d7b77b62e2124d0787737e2becb6adcf33f
wahid.ayan <wahid.sadique@sslwireless.com>
2020-12-15 13:10:17 +0600

    add fuel call

--- app/src/main/java/com/wsayan/retrofitdemo/MainActivity.kt
+++ app/src/main/java/com/wsayan/retrofitdemo/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.wsayan.retrofitdemo
 
 import BaseResponse
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.github.kittinunf.fuel.httpGet
+import com.google.gson.Gson
 import com.squareup.picasso.Picasso
 import com.wsayan.retrofitdemo.databinding.ActivityMainBinding
 import retrofit2.Call
@@ -18,28 +21,70 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, RES)
         apiInterface = RetrofitClient.apiClient?.create(ApiInterface::class.java)
-        loadUserInfo()
+
+        binding.retrofitButton.setOnClickListener {
+            loadUserInfoWithRetrofit()
+        }
+
+        binding.fuelButton.setOnClickListener {
+            loadUserInfoWithFuel()
+        }
     }
 
-    private fun loadUserInfo() {
+    private fun loadUserInfoWithRetrofit() {
+        clearFields()
         val call: Call<BaseResponse?>? = apiInterface?.getUserInfo()
         call?.enqueue(object : Callback<BaseResponse?> {
             override fun onResponse(call: Call<BaseResponse?>, response: Response<BaseResponse?>) {
                 val data = response.body()?.data
                 Picasso.get()
-                        .load(data?.avatar)
-                        .into(binding.ppImageView);
+                    .load(data?.avatar)
+                    .into(binding.ppImageView);
                 binding.nameTextView.text = "${data?.first_name} ${data?.last_name}"
                 binding.emailTextView.text = data?.email
             }
 
             override fun onFailure(call: Call<BaseResponse?>, t: Throwable) {
-
+                val throwable = t
+                Log.wtf(TAG, t.message)
             }
         })
     }
 
+    private fun loadUserInfoWithFuel() {
+        clearFields()
+        val httpAsync = "${BuildConfig.SERVER_BASE_URL}api/users/2"
+            .httpGet()
+            .responseString { request, response, result ->
+                result.fold(success = {
+                    Log.wtf(TAG, it)
+
+                    val gson = Gson()
+                    val responseData = gson.fromJson(it, BaseResponse::class.java)
+                    val data = responseData.data
+
+                    Picasso.get()
+                        .load(data.avatar)
+                        .into(binding.ppImageView);
+                    binding.nameTextView.text = "${data?.first_name} ${data?.last_name}"
+                    binding.emailTextView.text = data.email
+
+                }, failure = {
+                    Log.wtf(TAG, String(it.errorData))
+                })
+            }
+
+        httpAsync.join()
+    }
+
+    private fun clearFields() {
+        binding.ppImageView.setImageDrawable(null)
+        binding.nameTextView.text = ""
+        binding.emailTextView.text = ""
+    }
+
     companion object {
         const val RES = R.layout.activity_main
+        const val TAG = "MainActivity"
     }
 }
